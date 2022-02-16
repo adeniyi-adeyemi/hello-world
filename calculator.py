326eced32d81140f5855f432d5ecfe767638609d
Adeniyi Adeyemi <adeyemiolutoni@gmail.com>
2022-02-16 04:59:43 -0500

    new update calculator

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
-def calculator(num1, num2):
-    sum = num1 + num2
-    return sum
+def add(num1, num2):
+    result = num1 + num2
+    return result
 
 
 # num1 =  input("Enter a number: ")
 # num2 = input("Enter another number: ")
 
-result = calculator(2,5)
-print(result)
+sum = add(2,5)
+print(sum)
